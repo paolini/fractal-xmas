c265bd45897c22dfd3224654c2d659b382f21de6
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-12-20 11:15:18 +0100

    customization

--- xmas.js
+++ xmas.js
@@ -1,44 +1,71 @@
-
-function main($canvas) {
-    var ctx = $canvas.getContext('2d'); 
-    var snowFlakes = []
-
-    function draw() {
-        ctx.fillStyle = '#003c71';
+class Xmas {
+    constructor($canvas, options) {
+        this.ctx = $canvas.getContext('2d'); 
+        this.snowFlakes = []
+        this.options = {
+            nFlakes: 100,
+            fillColor: 'darkblue',
+            speed: 1.0,
+            minIterations: 3,
+            maxIterations: 5,
+            ...options }
+        this.width = $canvas.width;
+        this.height = $canvas.height;
+        this.start();
+    }
+    
+    draw() {
+        const snowFlakes = this.snowFlakes;
+        const ctx = this.ctx;
+        ctx.fillStyle = this.options.fillColor;
         ctx.fillRect(0, 0, $canvas.width, $canvas.height);
         for (var i = 0; i < snowFlakes.length; i++) {
             snowFlakes[i].draw(ctx);
         }
     }
 
-    function update() {
+    update() {
+        const snowFlakes = this.snowFlakes;
         for (var i = 0; i < snowFlakes.length; i++) {
             snowFlakes[i].update();
             if (snowFlakes[i].y > $canvas.height) {
                 snowFlakes.splice(i, 1);
+                i--;
             }
         }
-        if (snowFlakes.length < 100 && Math.random() < 0.1) {
-            snowFlakes.push(new Snowflake($canvas.width, $canvas.height));
+        if (snowFlakes.length < this.options.nFlakes) {
+            snowFlakes.push(new Snowflake(this.width, this.height, this.options));
         }
     }
 
-    function loop() {
-        requestAnimationFrame(loop);
-        update();
-        draw();
+    start() {
+        if (this.run) return;
+        this.run = true;
+        this.loop();
     }
 
-    loop(); 
+    stop() {
+        this.run = false;
+    }
+
+    loop() {
+        if (!this.run) return;
+        this.update();
+        this.draw();
+        requestAnimationFrame(() => this.loop());
+    }
 }
 
 class Snowflake {
-    constructor(width, height) {
+    constructor(width, height, options) {
         this.x = Math.random() * width;
         this.y = 0;
         this.angle = Math.random() * 2 * Math.PI;
         this.scale = Math.pow(Math.random(),4)*width/20+width/100;
-        this.iterations = 1+Math.round(Math.random() * 4);
+        this.options = options;
+        const minIterations = options.minIterations || 3;
+        const maxIterations = options.maxIterations || 5;
+        this.iterations = minIterations+Math.floor(Math.random() * (maxIterations - minIterations + 1));
         // random white-ish color   
         this.color = 'rgb(' + Math.round(256 - Math.random() * 128) + ',' + Math.round(256-Math.random() * 32) + ',' + Math.round(256 - Math.random() * 8) + ')';
         //this.color = 'white'
@@ -74,9 +101,9 @@ class Snowflake {
         ctx.restore();
     }
 
-    update() {
+    update(speed = 1) {
         this.angle += this.angleSpeed;
-        this.y += 0.05 * this.scale;
+        this.y += this.options.speed * 0.05 * this.scale;
     }
 
     kochIter(ctx, scale, iterations) {
